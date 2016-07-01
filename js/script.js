d41a7298496ab33cf4a530de4336080865f65086
dbrown <dbrown>
2016-07-01 10:32:20 -0400

    Updated a lot!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-$("#hide").click(function(){
-$("#hide").fadeOut(1000);
+/**$(document).ready(function(){
+$("#viewMoreItems").click(function(){
+$("#box").fadeIn(1000);
 });
-});
+});*/
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
